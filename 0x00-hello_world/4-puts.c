55783ff269cf4fdaae718770edacf73349f7f5ae
Artista001 <samwuels003@gmail.com>
2022-12-11 23:27:56 -0800

    betty trouble commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
-#!/bin/bash
 #include <stdio.h>
+/**
+ *main -puts a line code using puts
+ *
+ * Return: 0
+ */
 
 int main(void)
-
 {
-
-	    puts("Programming is like building a multilingual puzzle");
-			    return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
