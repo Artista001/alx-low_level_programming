dd9da5039de17a6ceae79707c5b208689b1efa23
Artista001 <samwuels003@gmail.com>
2022-12-14 09:55:27 -0800

    error commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * Checks_islower - entry point
+ *
+ * Return: always 0
+ */
+int  _islower(int c)
+{
+	('a' to 'z'),
+		if (c >= 'a' && c <= 'z')
+		{
+			return (1);
+		}
+	return (0);
+}
