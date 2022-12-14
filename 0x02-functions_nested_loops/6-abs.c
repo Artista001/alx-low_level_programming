d2e5b4cd8f27b30b77c5175fc05c481c8d13be1f
Artista001 <samwuels003@gmail.com>
2022-12-14 10:46:46 -0800

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - computes the abolute value of an integer
+ * @n: The character to be checked
+ * Return: Always 0
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
