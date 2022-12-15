6341f639f88357f8d480ee8173978ede2946db1d
Artista001 <samwuels003@gmail.com>
2022-12-15 14:22:59 -0800

    0x04.c commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,15 +1,27 @@
 #include "main.h"
 /**
  * print_last_digit - prints the last digit of a number
+ * _abs - prints the absolute value of an integer
  * @n: The character to be checked
  * Return: The value of the last digit
  */
 int print_last_digit(int n)
 {
-	int abs_n = _abs(n);
-	int last_digit = abs_n % 10;
+	int _abs(int n)
+	{
+		if (n < 0)
+		{
+			return (-n);
+		}
+		else
+		{
+			return (n);
+		}
+		int abs_n = _abs(n);
+		int last_digit = abs_n % 10;
 
-	_putchar(last_digit + '0');
+		_putchar(last_digit + '0');
 
-	return (last_digit);
+		return (last_digit);
+	}
 }
