c723ae13c50d679d517ddc637d4af5027bade62e
Artista001 <samwuels003@gmail.com>
2022-12-19 08:16:49 -0800

    new 0 commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,9 +2,9 @@
 /**
  * reset_to_98 - function that takes a pointer to an int as a parameter and
  * updates the value it points to 98
- * @n - This is the entry point
+ * @n: This is the entry point
  */
-void reset_to_98(int* n)
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
