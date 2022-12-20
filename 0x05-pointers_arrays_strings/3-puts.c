961e7bb0f8b51771d83909c69da46222e166df19
Artista001 <samwuels003@gmail.com>
2022-12-19 18:50:51 -0800

    Second dawn deadline

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - function that prints a string followed by a new line to stdout
+ * @str: This is the input string
+ */
+void _puts(char *str)
+{
+	int len;
+
+	for (len = 0; str[len] != '\0'; len++)
+	{
+		_putchar(str[index]);
+	}
+	_putchar('\n');
+}
