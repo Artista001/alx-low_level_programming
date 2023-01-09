f8b66990fd5c8b7f47fc74c1ccb1fa227e2f9e71
Artista001 <samwuels003@gmail.com>
2023-01-09 12:20:55 -0800

    change commit

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -36,7 +36,7 @@ int main(int argc, char *argv[])
 		{
 			aux = (total / coins[position]);
 			change += aux;
-			total = coins[position] * aux;
+			total -= coins[position] * aux;
 		}
 		position++;
 	}
