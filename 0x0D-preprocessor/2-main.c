3c57d207642624ef4277c3e14b1eded6c614338b
Artista001 <samwuels003@gmail.com>
2023-01-18 10:36:18 -0800

    main.c new commit

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 /**
- * main - prints the name of the file
+ * ain - prints the name of the file
  *
  * Return: Always 0 (Success)
  */
+
 int ain(void)
 {
 	printf("%s\n", _file_);
