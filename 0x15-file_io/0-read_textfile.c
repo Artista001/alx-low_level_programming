171ee1295a50db40169854d2976af22e0eb3460f
Artista001 <samwuels003@gmail.com>
2023-02-08 13:17:21 -0800

    task 0 corrections

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,10 +11,8 @@
  * @letters: number of letters it should read and print
  *
  * Return: actual number of letters it could read and print
- *
  */
-
-ssize_t read_textfile(const char *filename, size_t letters);
+ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t read_count, write_count;
@@ -22,33 +20,25 @@ ssize_t read_textfile(const char *filename, size_t letters);
 
 	if (filename == NULL)
 		return (0);
-
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-
 	buf = malloc(sizeof(char) * letters);
-	if (!buf)
+	if (buf == NULL)
 	{
 		close(fd);
 		return (0);
 	}
-
 	read_count = read(fd, buf, letters);
+	close(fd);
 	if (read_count == -1)
 	{
-		close(fd);
 		free(buf);
-
 		return (0);
 	}
-
 	write_count = write(STDOUT_FILENO, buf, read_count);
-	close(fd);
 	free(buf);
-
-	if (write_count != read_count)
+	if (read_count != write_count)
 		return (0);
-
-	return (read_count);
+	return (write_count);
 }
